46e4ce25b95635beae0aba10a9f05718def3c919
Robert Walker <1700618@uad.ac.uk>
2021-03-01 15:23:44 +0000

    Handle Rotation
    Created an executable to do rotation in a similar fashion to the handle movement block.
    
    Updated the handle movement widget to draw connections.

--- Source/InteractiveScripting/Executable/HandleRotation.cpp
+++ Source/InteractiveScripting/Executable/HandleRotation.cpp
@@ -1,5 +1,111 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "HandleRotation.h"
 
+AHandleRotation::AHandleRotation()
+{
+	rotLeft = EKeys::Q;
+	rotRight = EKeys::E;
+}
+
+AExecutable::ExecuteResult AHandleRotation::Execute()
+{
+	if (!rotationSpeed->IsValidLowLevel())
+	{
+		rotationSpeed = nullptr;
+		return MissingComponent;
+	}
+
+	if (!deltaTime->IsValidLowLevel())
+	{
+		deltaTime = nullptr;
+		return MissingComponent;
+	}
+
+	if (!nextLine->IsValidLowLevel())
+	{
+		nextLine = nullptr;
+	}
+
+	if (HasPlayerController() && HasVarManager())
+		DoRotationControls();
+
+	if (nextLine == nullptr)
+		return EndReached;
+	else
+		return nextLine->Execute();
+}
+
+bool AHandleRotation::HasPlayerController()
+{
+	if (controller->IsValidLowLevel())
+	{
+		return true;
+	}
+	else
+	{
+		controller = GetWorld()->GetFirstPlayerController();
+
+		if (controller->IsValidLowLevel())
+		{
+			return true;
+		}
+		else
+		{
+			controller = nullptr;
+			return false;
+		}
+	}
+}
+
+bool AHandleRotation::HasVarManager()
+{
+	if (varManager->IsValidLowLevel())
+	{
+		return true;
+	}
+	else
+	{
+		AActor* owner = GetAttachParentActor();
+		if (owner->IsA<ABlockManager>())
+		{
+			ABlockManager* blockManager = (ABlockManager*)owner;
+			varManager = blockManager->GetVarManager();
+
+			//Check to ensure we found the variable manager
+			if (varManager->IsValidLowLevel())
+				return true;
+		}
+	}
+
+	return false;
+}
+
+void AHandleRotation::DoRotationControls()
+{
+	float rotAmount = rotationSpeed->Evaluate() * deltaTime->Evaluate();
+
+	if (controller->IsInputKeyDown(rotLeft))
+	{
+		float rotation = varManager->GetNumber(Variables::Rotation);
+		rotation -= rotAmount;
+		varManager->SetNumber(Variables::Rotation, rotation);
+	}
+
+	if (controller->IsInputKeyDown(rotRight))
+	{
+		float rotation = varManager->GetNumber(Variables::Rotation);
+		rotation += rotAmount;
+		varManager->SetNumber(Variables::Rotation, rotation);
+	}
+
+}
+
+void AHandleRotation::SetRotationSpeedAsSlot(AConnectionManager* connectionManager)
+{
+	connectionManager->AssignNumSlot(&rotationSpeed);
+}
+
+void AHandleRotation::SetDeltaTimeAsSlot(AConnectionManager* connectionManager)
+{
+	connectionManager->AssignNumSlot(&deltaTime);
+}
--- Source/InteractiveScripting/Executable/HandleRotation.h
+++ Source/InteractiveScripting/Executable/HandleRotation.h
@@ -1,17 +1,40 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "CoreMinimal.h"
-#include "Executable/Executable.h"
+#include "Executable.h"
+#include "InteractiveScripting/Evaluatable/Number.h"
+#include "InteractiveScripting/BlockManager.h"
+#include "InteractiveScripting/VariableManager.h"
 #include "HandleRotation.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class INTERACTIVESCRIPTING_API AHandleRotation : public AExecutable
 {
 	GENERATED_BODY()
 	
+public:
+	AHandleRotation();
+
+protected:
+
+	bool HasPlayerController();
+	bool HasVarManager();
+	void DoRotationControls();
+
+	APlayerController* controller;
+	UVariableManager* varManager;
+	FKey rotLeft, rotRight;
+
+	ANumber* rotationSpeed;
+	ANumber* deltaTime;
+
+public:
+
+	ExecuteResult Execute() override;
+
+	UFUNCTION(BlueprintCallable)
+		void SetRotationSpeedAsSlot(AConnectionManager* connectionManager);
+
+	UFUNCTION(BlueprintCallable)
+		void SetDeltaTimeAsSlot(AConnectionManager* connectionManager);
 };
