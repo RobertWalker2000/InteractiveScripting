1bdcdfe81f6f28a34ad5461e822bb8a21b3e8700
Robert Walker <1700618@uad.ac.uk>
2021-03-03 15:21:18 +0000

    Shoot class
    Added a shoot executable class which will handle the firing of the projectile.
    
    Class still needs work, will currently shoot every frame it is called, when we really only want it to shoot on a certain cooldown.

--- Source/InteractiveScripting/Executable/Shoot.cpp
+++ Source/InteractiveScripting/Executable/Shoot.cpp
@@ -1,5 +1,70 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Shoot.h"
+#include "InteractiveScripting/Blockmanager.h"
+#include "InteractiveScripting/Projectile.h"
 
+AShoot::AShoot()
+{
+
+}
+
+AExecutable::ExecuteResult AShoot::Execute()
+{
+	if (!nextLine->IsValidLowLevel())
+	{
+		nextLine = nullptr;
+	}
+
+	if (HasVarManager())
+	{
+		positionX = varManager->GetNumber(Variables::PositionX);
+		positionY = varManager->GetNumber(Variables::PositionY);
+		rotation = varManager->GetNumber(Variables::Rotation);
+		speed = 60.0f;
+	}
+
+	FTransform spawnTransform = FTransform::Identity;
+
+	FVector position = FVector(positionX, positionY, 70.0f);
+
+	//Create a vector representing the forward vector of the player based on the inputs
+	FVector direction = FVector::RightVector;
+	direction = direction.RotateAngleAxis(rotation, FVector::UpVector);
+	direction.Normalize();
+
+	//Move the spawn position along the forward vector a small amount to ensure it is outside the player
+	position += (direction * 60.0f);
+	spawnTransform.SetTranslation(position);
+
+	FVector ballVel = direction * 60.0f;
+	AProjectile* projectile = GetWorld()->SpawnActor<AProjectile>(AProjectile::StaticClass(), spawnTransform);
+	projectile->ballMesh->AddImpulse(ballVel, FName(), true);
+
+	if (nextLine == nullptr)
+		return EndReached;
+	else
+		return nextLine->Execute();
+}
+
+bool AShoot::HasVarManager()
+{
+	if (varManager->IsValidLowLevel())
+	{
+		return true;
+	}
+	else
+	{
+		AActor* owner = GetAttachParentActor();
+		if (owner->IsA<ABlockManager>())
+		{
+			ABlockManager* blockManager = (ABlockManager*)owner;
+			varManager = blockManager->GetVarManager();
+
+			//Check to ensure we found the variable manager
+			if (varManager->IsValidLowLevel())
+				return true;
+		}
+	}
+
+	return false;
+}
--- Source/InteractiveScripting/Executable/Shoot.h
+++ Source/InteractiveScripting/Executable/Shoot.h
@@ -1,17 +1,25 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "CoreMinimal.h"
-#include "Executable/Executable.h"
+#include "Executable.h"
+#include "InteractiveScripting/VariableManager.h"
+#include "InteractiveScripting/Projectile.h"
 #include "Shoot.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class INTERACTIVESCRIPTING_API AShoot : public AExecutable
 {
 	GENERATED_BODY()
 	
+public:
+	AShoot();
+
+protected:
+	UVariableManager* varManager;
+
+	float positionX = 0, positionY = 0, rotation = 0, speed = 0;
+
+	bool HasVarManager();
+public:
+	ExecuteResult Execute() override;
 };
